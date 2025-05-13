c593cbd88b2c06a6d2cee6797496d07fc39fe19f
AB MisHkat <mishkat606@gmail.com>
2025-05-13 13:10:28 +0600

    apis update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose
   .catch((err) => console.error("⚠️ MongoDB connection error:", err));
 
 // Import Routes
-const businessRoutes = require("./routes/businessRoutes");
+const businessRoutes = require("./api/businessRoutes");
 
 // Use Routes
 app.use("/api/b2bi", businessRoutes);
